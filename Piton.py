0d5b3699060cd49d326156ed8f7bf73e12df5a0d
mehremic1 <mehremic.a@gmail.com>
2019-02-28 14:44:31 +0100

    Add colors for game

--- Piton.py
+++ Piton.py
@@ -16,5 +16,11 @@ else:
 # Play surface
 playSurface = pygame.display.set_mode((720, 460))
 pygame.display.set_caption('Piton')
-time.sleep(5)
 
+# Colors
+red = pygame.Color(255, 0, 0) # for game over
+green = pygame.Color(0, 255, 0) # for snake
+blue = pygame.Color(0, 0, 255)
+black = pygame.Color(0, 0, 0) # for score
+white = pygame.Color(255, 255, 255) # for background
+brown = pygame.Color(165, 42, 42) # for food
